1f32173cc9fb3bb10a9a6c25b7354bcbb012276f
sampathshetti <125035217+sampathshetti@users.noreply.github.com>
2023-02-19 17:06:06 +0530

    Update UsersDBConstants.java

--- src/main/java/constants/db/UsersDBConstants.java
+++ src/main/java/constants/db/UsersDBConstants.java
@@ -4,8 +4,8 @@ public interface UsersDBConstants {
 
 	public static String TABLE_USERS = "users";
 	
-	public static String COLUMN_PASSWORD = "username";
-	public static String COLUMN_USERNAME = "password";
+	public static String COLUMN_USERNAME = "username";
+	public static String COLUMN_PASSWORD = "password";
 	public static String COLUMN_FIRSTNAME = "firstname";
 	public static String COLUMN_LASTNAME = "lastname";
 	public static String COLUMN_ADDRESS = "address";
